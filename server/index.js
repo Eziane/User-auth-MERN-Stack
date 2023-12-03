b8e3d0e4463278597469c2cd5a18d2a1c21034e7
Eziane <ezianabderrahim@gmail.com>
2023-12-03 22:12:44 +0100

    connecting mongodb, storing user data

--- server/index.js
+++ server/index.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user.model')
 
-app.get('/', (req, res) =>{
-    res.send('hello world')
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+app.post('/api/register', async (req, res) =>{
+    console.log(req.body)
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({status: 'ok'})
+    } catch (err) {
+        console.log(err)
+        res.json({ status: 'error', error: 'Duplicate email'})
+    }
+})
+
+app.post('/api/login', async (req, res) =>{
+        const user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        })
+        if (user){
+            return res.json({ status: 'ok', user: true})
+        }else{
+            return res.json({ status: 'error', user: false})
+        }
 })
 
 app.listen(1337, () => {
